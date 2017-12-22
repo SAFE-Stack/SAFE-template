cd02a8177bd7ce364baaf44361897eeb116e5ad9
Tomasz Heimowski <tomek.heimowski@gmail.com>
2017-12-22 19:28:36 +0100

    first step - hello world from giraffe

--- Content/src/Server/Program.fs
+++ Content/src/Server/Program.fs
@@ -1,45 +1,26 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
-open System.Net
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
 
-open Suave
-open Suave.Operators
+open Giraffe
 
-open Shared
-
-#if (Remoting)
-open Fable.Remoting.Suave
-#endif
-
-let clientPath = Path.Combine("..","Client") |> Path.GetFullPath 
+let clientPath = Path.Combine("..","Client") |> Path.GetFullPath
 let port = 8085us
 
-let config =
-  { defaultConfig with 
-      homeFolder = Some clientPath
-      bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") port ] }
+let webApp = text "hello world"
 
-let getInitCounter () : Async<Counter> = async { return 42 }
+let configureApp  (app : IApplicationBuilder) =
+  app.UseStaticFiles()
+     .UseGiraffe webApp
 
-let init : WebPart = 
-#if (Remoting)
-  let counterProcotol = 
-    { getInitCounter = getInitCounter }
-  // creates a WebPart for the given implementation
-  FableSuaveAdapter.webPartWithBuilderFor counterProcotol Route.builder
-#else
-  Filters.path "/api/init" >=>
-  fun ctx ->
-    async {
-      let! counter = getInitCounter()
-      return! Successful.OK (string counter) ctx
-    }
-#endif
-
-let webPart =
-  choose [
-    init
-    Files.browseHome
-  ]
-
-startWebServer config webPart
+WebHost
+  .CreateDefaultBuilder()
+  .UseWebRoot(clientPath)
+  .UseContentRoot(clientPath)
+  .Configure(Action<IApplicationBuilder> configureApp)
+  .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+  .Build()
+  .Run()
---
+++ Content/src/Server/ProgramSuave.fs
@@ -0,0 +1,45 @@
+﻿open System.IO
+
+open System.Net
+
+open Suave
+open Suave.Operators
+
+open Shared
+
+#if (Remoting)
+open Fable.Remoting.Suave
+#endif
+
+let clientPath = Path.Combine("..","Client") |> Path.GetFullPath 
+let port = 8085us
+
+let config =
+  { defaultConfig with 
+      homeFolder = Some clientPath
+      bindings = [ HttpBinding.create HTTP (IPAddress.Parse "0.0.0.0") port ] }
+
+let getInitCounter () : Async<Counter> = async { return 42 }
+
+let init : WebPart = 
+#if (Remoting)
+  let counterProcotol = 
+    { getInitCounter = getInitCounter }
+  // creates a WebPart for the given implementation
+  FableSuaveAdapter.webPartWithBuilderFor counterProcotol Route.builder
+#else
+  Filters.path "/api/init" >=>
+  fun ctx ->
+    async {
+      let! counter = getInitCounter()
+      return! Successful.OK (string counter) ctx
+    }
+#endif
+
+let webPart =
+  choose [
+    init
+    Files.browseHome
+  ]
+
+startWebServer config webPart
