cc269fdd3909f887c955722388d4bd11cc2bec41
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-01-07 09:53:04 +0100

    split big function into smaller

--- Content/src/Client/App.fs
+++ Content/src/Client/App.fs
@@ -19,8 +19,8 @@ open Fulma.BulmaClasses
 
 #if (Fulma == "landing")
 open Fulma.BulmaClasses.Bulma
-open Fulma.Extra.FontAwesome
 open Fulma.BulmaClasses.Bulma.Properties
+open Fulma.Extra.FontAwesome
 #endif
 
 type Model = Counter option
@@ -88,6 +88,9 @@ let safeComponents =
 #if (Fulma != "none")
       "Fulma", "https://mangelmaxime.github.io/Fulma" 
 #endif
+#if (Fulma == "landing")
+      "Bulma\u00A0Templates", "https://dansup.github.io/bulma-templates/"
+#endif
 #if (Remoting)
       "Fable.Remoting", "https://github.com/Zaid-Ajaj/Fable.Remoting"
 #endif
@@ -141,38 +144,63 @@ let view model dispatch =
         [ Content.content [ Content.customClass Bulma.Level.Item.HasTextCentered ]
             [ safeComponents ] ] ]
 #else
+let navBrand =
+  Navbar.brand_div [ ] 
+    [ Navbar.item_a 
+        [ Navbar.Item.props [ Href "https://safe-stack.github.io/" ]
+          Navbar.Item.isActive ] 
+        [ img [ Src "https://safe-stack.github.io/images/safe_top.png"
+                Alt "Logo" ] ] 
+      Navbar.burger [ ] 
+        [ span [ ] [ ]
+          span [ ] [ ]
+          span [ ] [ ] ] ]
+
+let navMenu =
+  Navbar.menu [ ]
+    [ Navbar.end_div [ ] 
+        [ Navbar.item_a [ ] 
+            [ str "Home" ] 
+          Navbar.item_a [ ]
+            [ str "Examples" ]
+          Navbar.item_a [ ]
+            [ str "Documentation" ]
+          Navbar.item_div [ ]
+            [ Button.button_a 
+                [ Button.isWhite
+                  Button.isOutlined
+                  Button.isSmall
+                  Button.props [ Href "https://github.com/SAFE-Stack/SAFE-template" ] ] 
+                [ Icon.faIcon [ ] 
+                    [ Fa.icon Fa.I.Github; Fa.fw ]
+                  span [ ] [ str "View Source" ] ] ] ] ]
+
+let containerBox model dispatch =
+  Box.box' [ ]
+    [ Form.Field.field_div [ Form.Field.isGrouped ] 
+        [ Form.Control.control_p [ Form.Control.customClass "is-expanded"] 
+            [ Form.Input.input
+                [ Form.Input.typeIsNumber
+                  Form.Input.disabled true
+                  Form.Input.value (show model) ] ]
+          Form.Control.control_p [ ]
+            [ Button.button_a 
+                [ Button.isPrimary
+                  Button.onClick (fun _ -> dispatch Increment) ]
+                [ str "+" ] ]
+          Form.Control.control_p [ ]
+            [ Button.button_a 
+                [ Button.isPrimary
+                  Button.onClick (fun _ -> dispatch Decrement) ]
+                [ str "-" ] ] ] ]
+
 let view model dispatch =
   Hero.hero [ Hero.isPrimary; Hero.isFullHeight ] 
     [ Hero.head [ ] 
         [ Navbar.navbar [ ]
             [ Container.container [ ]
-                [ Navbar.brand_div [ ] 
-                    [ Navbar.item_a 
-                        [ Navbar.Item.props [ Href "https://safe-stack.github.io/" ]
-                          Navbar.Item.isActive ] 
-                        [ img [ Src "https://safe-stack.github.io/images/safe_top.png"
-                                Alt "Logo" ] ] 
-                      Navbar.burger [ ] 
-                        [ span [ ] [ ]
-                          span [ ] [ ]
-                          span [ ] [ ] ] ]
-                  Navbar.menu [ ]
-                    [ Navbar.end_div [ ] 
-                        [ Navbar.item_a [ ] 
-                            [ str "Home" ] 
-                          Navbar.item_a [ ]
-                            [ str "Examples" ]
-                          Navbar.item_a [ ]
-                            [ str "Documentation" ]
-                          Navbar.item_div [ ]
-                            [ Button.button_a 
-                                [ Button.isWhite
-                                  Button.isOutlined
-                                  Button.isSmall
-                                  Button.props [ Href "https://github.com/SAFE-Stack/SAFE-template" ] ] 
-                                [ Icon.faIcon [ ] 
-                                    [ Fa.icon Fa.I.Github; Fa.fw ]
-                                  span [ ] [ str "View Source" ] ] ] ] ] ] ] ]
+                [ navBrand
+                  navMenu ] ] ]
       
       Hero.body [ ] 
         [ Container.container [ Container.customClass Alignment.HasTextCentered ]
@@ -181,25 +209,9 @@ let view model dispatch =
                   Column.Offset.is3 ]
                 [ h1 [ ClassName "title" ] 
                     [ str "SAFE Template" ]
-                  h2 [ ClassName "subtitle" ]
+                  div [ ClassName "subtitle" ]
                     [ safeComponents ]
-                  Box.box' [ ]
-                    [ Form.Field.field_div [ Form.Field.isGrouped ] 
-                        [ Form.Control.control_p [ Form.Control.customClass "is-expanded"] 
-                            [ Form.Input.input
-                                [ Form.Input.typeIsNumber
-                                  Form.Input.disabled true
-                                  Form.Input.value (show model) ] ]
-                          Form.Control.control_p [ ]
-                            [ Button.button_a 
-                                [ Button.isPrimary
-                                  Button.onClick (fun _ -> dispatch Increment) ]
-                                [ str "+" ] ]
-                          Form.Control.control_p [ ]
-                            [ Button.button_a 
-                                [ Button.isPrimary
-                                  Button.onClick (fun _ -> dispatch Decrement) ]
-                                [ str "-" ] ] ] ] ] ] ] ]
+                  containerBox model dispatch ] ] ] ]
 #endif
 
   
