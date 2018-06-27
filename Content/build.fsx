0d818794bca16de84bd5158307dc717015916dcd
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-06-27 11:27:26 +0200

    add "#r Facades/netstandard" to fake scripts to fix intellisense on Mono
    Related issue https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095

--- Content/build.fsx
+++ Content/build.fsx
@@ -4,6 +4,7 @@
 -:cnd:noEmit
 #if !FAKE
 #r "netstandard"
+#r "Facades/netstandard" // https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095
 #endif
 +:cnd:noEmit
 
--- build.fsx
+++ build.fsx
@@ -3,6 +3,7 @@
 
 #if !FAKE
 #r "netstandard"
+#r "Facades/netstandard" // https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095
 #endif
 
 open Fake.Core
