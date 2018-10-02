7c58419035515ba7a9837407b741b0ed2e1c5506
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-10-02 13:08:32 +0200

    Update configuration file as per https://github.com/SAFE-Stack/SAFE-BookStore/pull/349

--- Content/src/Client/webpack.config.js
+++ Content/src/Client/webpack.config.js
@@ -1,76 +1,116 @@
+// https://github.com/SAFE-Stack/SAFE-BookStore/blob/33efc0fab24c8785e244c5966c54fce2ba03e65a/src/Client/webpack.config.js
 var path = require("path");
-var webpack = require("webpack");
-var fableUtils = require("fable-utils");
 
 function resolve(filePath) {
     return path.join(__dirname, filePath)
 }
 
-var babelOptions = fableUtils.resolveBabelOptions({
-    presets: [
-        ["env", {
-            "targets": {
-                "browsers": ["last 2 versions"]
-            },
-            "modules": false
-        }]
-    ],
-    plugins: ["transform-runtime"]
-});
-
+var CONFIG = {
+    fsharpEntry:
+        ["whatwg-fetch",
+            "@babel/polyfill",
+            resolve("./Client.fsproj")
+        ],
+    outputDir: resolve("./public"),
+    devServerPort: undefined,
+    devServerProxy: {
+        '/api/*': {
+            target: 'http://localhost:' + (process.env.SUAVE_FABLE_PORT || "8085"),
+            changeOrigin: true
+        }
+    },
+    historyApiFallback: {
+        index: resolve("./index.html")
+    },
+    contentBase: __dirname,
+    // Use babel-preset-env to generate JS compatible with most-used browsers.
+    // More info at https://github.com/babel/babel/blob/master/packages/babel-preset-env/README.md
+    babel: {
+        presets: [
+            ["@babel/preset-env", {
+                "targets": {
+                    "browsers": ["last 2 versions"]
+                },
+                "modules": false,
+                "useBuiltIns": "usage",
+            }]
+        ],
+        plugins: ["@babel/plugin-transform-runtime"]
+    }
+}
 
 var isProduction = process.argv.indexOf("-p") >= 0;
-var port = process.env.SUAVE_FABLE_PORT || "8085";
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
+var path = require("path");
+var webpack = require("webpack");
+var MinifyPlugin = require("terser-webpack-plugin");
+
+var commonPlugins = [
+];
+
 module.exports = {
-    devtool: "source-map",
-    entry : [ "whatwg-fetch", "babel-polyfill", resolve('./Client.fsproj')],
-    mode: isProduction ? "production" : "development",
+    entry: CONFIG.fsharpEntry,
+    // NOTE we add a hash to the output file name in production
+    // to prevent browser caching if code changes
     output: {
-        path: resolve('./public/js'),
-        publicPath: "/js",
-        filename: "bundle.js"
+        path: CONFIG.outputDir,
+        publicPath: "/public",
+        filename: '[name].js'
     },
     resolve: {
         symlinks: false,
-        modules: [resolve("../../node_modules/")]
     },
-    devServer: {
-        proxy: {
-            '/api/*': {
-                target: 'http://localhost:' + port,
-                changeOrigin: true
+    mode: isProduction ? "production" : "development",
+    devtool: isProduction ? undefined : "source-map",
+    optimization: {
+        // Split the code coming from npm packages into a different file.
+        // 3rd party dependencies change less often, let the browser cache them.
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /node_modules/,
+                    name: "vendors",
+                    chunks: "all"
+                }
             }
         },
-        contentBase: "./public",
-        hot: true,
-        inline: true
+        minimizer: isProduction ? [new MinifyPlugin()] : []
     },
+    // Besides the HtmlPlugin, we use the following plugins:
+    // PRODUCTION
+    //      - UglifyJSPlugin: Minimize the CSS
+    // DEVELOPMENT
+    //      - HotModuleReplacementPlugin: Enables hot reloading when code changes without refreshing
+    plugins: isProduction ?
+        commonPlugins
+        : commonPlugins.concat([
+            new webpack.HotModuleReplacementPlugin(),
+        ]),
+    // Configuration for webpack-dev-server
+    devServer: {
+        proxy: CONFIG.devServerProxy,
+        hot: true,
+        inline: true,
+        historyApiFallback: CONFIG.historyApiFallback,
+        contentBase: CONFIG.contentBase
+    },
+    // - fable-loader: transforms F# into JS
+    // - babel-loader: transforms JS to old syntax (compatible with old browsers)
     module: {
         rules: [
             {
                 test: /\.fs(x|proj)?$/,
-                use: {
-                    loader: "fable-loader",
-                    options: {
-                        babel: babelOptions,
-                        define: isProduction ? [] : ["DEBUG"]
-                    }
-                }
+                use: "fable-loader"
             },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
-                    options: babelOptions
+                    options: CONFIG.babel
                 },
             }
         ]
-    },
-    plugins: isProduction ? [] : [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin()
-    ]
+    }
 };
