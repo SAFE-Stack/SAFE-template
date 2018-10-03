366dc7cf277576a0ff99a29237e97e38a74fe586
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-10-03 13:10:25 +0200

    Make the webpack config work for dev-server for default template

--- Content/src/Client/webpack.config.js
+++ Content/src/Client/webpack.config.js
@@ -1,18 +1,16 @@
-// https://github.com/SAFE-Stack/SAFE-BookStore/blob/33efc0fab24c8785e244c5966c54fce2ba03e65a/src/Client/webpack.config.js
 var path = require("path");
+var webpack = require("webpack");
 
 function resolve(filePath) {
     return path.join(__dirname, filePath)
 }
 
 var CONFIG = {
-    fsharpEntry:
-        ["whatwg-fetch",
-            "@babel/polyfill",
-            resolve("./Client.fsproj")
-        ],
-    outputDir: resolve("./public"),
-    devServerPort: undefined,
+    fsharpEntry: [
+        "whatwg-fetch",
+        "@babel/polyfill",
+        resolve("./Client.fsproj")
+    ],
     devServerProxy: {
         '/api/*': {
             target: 'http://localhost:' + (process.env.SUAVE_FABLE_PORT || "8085"),
@@ -22,7 +20,7 @@ var CONFIG = {
     historyApiFallback: {
         index: resolve("./index.html")
     },
-    contentBase: __dirname,
+    contentBase: resolve("./public"),
     // Use babel-preset-env to generate JS compatible with most-used browsers.
     // More info at https://github.com/babel/babel/blob/master/packages/babel-preset-env/README.md
     babel: {
@@ -42,51 +40,19 @@ var CONFIG = {
 var isProduction = process.argv.indexOf("-p") >= 0;
 console.log("Bundling for " + (isProduction ? "production" : "development") + "...");
 
-var path = require("path");
-var webpack = require("webpack");
-var MinifyPlugin = require("terser-webpack-plugin");
-
-var commonPlugins = [
-];
-
 module.exports = {
-    entry: CONFIG.fsharpEntry,
-    // NOTE we add a hash to the output file name in production
-    // to prevent browser caching if code changes
+    devtool: isProduction ? undefined : "source-map",
+    entry : CONFIG.fsharpEntry,
+    mode: isProduction ? "production" : "development",
     output: {
-        path: CONFIG.outputDir,
-        publicPath: "/public",
-        filename: '[name].js'
+        path: resolve('./public/js'),
+        publicPath: "/js",
+        filename: "bundle.js"
     },
     resolve: {
         symlinks: false,
+        modules: [resolve("../../node_modules/")]
     },
-    mode: isProduction ? "production" : "development",
-    devtool: isProduction ? undefined : "source-map",
-    optimization: {
-        // Split the code coming from npm packages into a different file.
-        // 3rd party dependencies change less often, let the browser cache them.
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /node_modules/,
-                    name: "vendors",
-                    chunks: "all"
-                }
-            }
-        },
-        minimizer: isProduction ? [new MinifyPlugin()] : []
-    },
-    // Besides the HtmlPlugin, we use the following plugins:
-    // PRODUCTION
-    //      - UglifyJSPlugin: Minimize the CSS
-    // DEVELOPMENT
-    //      - HotModuleReplacementPlugin: Enables hot reloading when code changes without refreshing
-    plugins: isProduction ?
-        commonPlugins
-        : commonPlugins.concat([
-            new webpack.HotModuleReplacementPlugin(),
-        ]),
     // Configuration for webpack-dev-server
     devServer: {
         proxy: CONFIG.devServerProxy,
@@ -112,5 +78,9 @@ module.exports = {
                 },
             }
         ]
-    }
+    },
+    plugins: isProduction ? [] : [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin()
+    ]
 };
