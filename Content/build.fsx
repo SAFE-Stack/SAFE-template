f43297bbe82c9bc3fa6766ddc0693d70a5bb08f3
Isaac Abraham <isaac.abraham@gmail.com>
2019-08-26 16:10:10 +0200

    Support for tenantId.

--- Content/build.fsx
+++ Content/build.fsx
@@ -262,11 +262,14 @@ Target.create "ArmTemplate" (fun _ ->
     let authCtx =
         // You can safely replace these with your own subscription and client IDs hard-coded into this script.
         let subscriptionId = try Environment.environVar "subscriptionId" |> Guid.Parse with _ -> failwith "Invalid Subscription ID. This should be your Azure Subscription ID."
-        let clientId = try Environment.environVar "clientId" |> Guid.Parse with _ -> failwith "Invalid Client ID. This should be the Client ID of a Native application registered in Azure with permission to create resources in your subscription."
+        let clientId = try Environment.environVar "clientId" |> Guid.Parse with _ -> failwith "Invalid Client ID. This should be the Client ID of an application registered in Azure with permission to create resources in your subscription."
+        let tenantId =
+            try Environment.environVarOrNone "tenantId" |> Option.map Guid.Parse
+            with _ -> failwith "Invalid TenantId ID. This should be the Tenant ID of an application registered in Azure with permission to create resources in your subscription."
 
         Trace.tracefn "Deploying template '%s' to resource group '%s' in subscription '%O'..." armTemplate resourceGroupName subscriptionId
         subscriptionId
-        |> authenticateDevice Trace.trace { ClientId = clientId; TenantId = None }
+        |> authenticateDevice Trace.trace { ClientId = clientId; TenantId = tenantId }
         |> Async.RunSynchronously
 
     let deployment =
