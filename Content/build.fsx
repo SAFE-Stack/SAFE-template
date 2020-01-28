9a35182b76525f97ad8aaf741c17885f194af4a2
Tomasz Heimowski <tomek.heimowski@gmail.com>
2020-01-28 14:43:36 +0100

    runTool - always return result

--- Content/build.fsx
+++ Content/build.fsx
@@ -56,12 +56,14 @@ let gcloudTool () = platformTool "gcloud" "gcloud.cmd"
 
 let runTool cmd args workingDir =
     let arguments = args |> String.split ' ' |> Arguments.OfArgs
-    Command.RawCommand (cmd, arguments)
-    |> CreateProcess.fromCommand
-    |> CreateProcess.withWorkingDirectory workingDir
-    |> CreateProcess.ensureExitCode
-    |> Proc.run
-    |> ignore
+    let result =
+        Command.RawCommand (cmd, arguments)
+        |> CreateProcess.fromCommand
+        |> CreateProcess.withWorkingDirectory workingDir
+        |> CreateProcess.ensureExitCode
+        |> CreateProcess.redirectOutput
+        |> Proc.run
+    result.Result.Output |> (fun s -> s.TrimEnd())
 
 let runDotNet cmd workingDir =
     let result =
@@ -76,21 +78,9 @@ let openBrowser url =
     |> Proc.run
     |> ignore
 
-//#if (deploy == "gcp-kubernetes" || deploy == "heroku")
-let runToolWithOutput cmd args workingDir =
-    let arguments = args |> String.split ' ' |> Arguments.OfArgs
-    let result =
-        Command.RawCommand (cmd, arguments)
-        |> CreateProcess.fromCommand
-        |> CreateProcess.withWorkingDirectory workingDir
-        |> CreateProcess.ensureExitCode
-        |> CreateProcess.redirectOutput
-        |> Proc.run
-    result.Result.Output |> (fun s -> s.TrimEnd())
-//#endif
 //#if (deploy == "gcp-kubernetes")
 let getGcloudProject() =
-    runToolWithOutput (gcloudTool ()) "config get-value project -q" __SOURCE_DIRECTORY__
+    runTool (gcloudTool ()) "config get-value project -q" __SOURCE_DIRECTORY__
 
 let getDockerTag() = "v1"
 
@@ -100,20 +90,20 @@ let createDockerImageName projectName =
     sprintf "gcr.io/%s/%s:%s" projectId projectName dockerTag
 
 let deployExists appName =
-    let result = runToolWithOutput "kubectl" "get deploy" __SOURCE_DIRECTORY__
+    let result = runTool "kubectl" "get deploy" __SOURCE_DIRECTORY__
     let pattern = "^" + appName + "\s+"
     Regex.IsMatch(result, pattern, RegexOptions.Multiline)
 
 let updateKubernetesDeploy appName dockerTag =
     let updateArgs = sprintf "set image deployment/%s %s=%s" appName appName dockerTag
-    runTool "kubectl" updateArgs __SOURCE_DIRECTORY__
+    runTool "kubectl" updateArgs __SOURCE_DIRECTORY__ |> ignore
 
 let createAndExposeKubernetesDeploy appName dockerTag port =
     let deployArgs = sprintf "run %s --image=%s --port %i" appName dockerTag port
-    runTool "kubectl" deployArgs __SOURCE_DIRECTORY__
+    runTool "kubectl" deployArgs __SOURCE_DIRECTORY__ |> ignore
 
     let exposeArgs = sprintf "expose deployment %s --type=LoadBalancer --port 80 --target-port %i" appName port
-    runTool "kubectl" exposeArgs __SOURCE_DIRECTORY__
+    runTool "kubectl" exposeArgs __SOURCE_DIRECTORY__ |> ignore
 //#endif
 
 Target.create "Clean" (fun _ ->
@@ -124,15 +114,15 @@ Target.create "Clean" (fun _ ->
 
 Target.create "InstallClient" (fun _ ->
     printfn "Node version:"
-    runTool nodeTool "--version" __SOURCE_DIRECTORY__
+    runTool nodeTool "--version" __SOURCE_DIRECTORY__ |> ignore
 //#if (js-deps == "npm")
     printfn "Npm version:"
-    runTool npmTool "--version"  __SOURCE_DIRECTORY__
-    runTool npmTool "install" __SOURCE_DIRECTORY__
+    runTool npmTool "--version"  __SOURCE_DIRECTORY__ |> ignore
+    runTool npmTool "install" __SOURCE_DIRECTORY__ |> ignore
 //#else
     printfn "Yarn version:"
-    runTool yarnTool "--version" __SOURCE_DIRECTORY__
-    runTool yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
+    runTool yarnTool "--version" __SOURCE_DIRECTORY__ |> ignore
+    runTool yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__ |> ignore
 //#endif
 )
 
@@ -144,9 +134,9 @@ Target.create "Build" (fun _ ->
         System.Text.Encoding.UTF8
         (Path.combine clientPath "Version.fs")
 //#if (js-deps == "npm")
-    runTool npxTool "webpack-cli -p" __SOURCE_DIRECTORY__
+    runTool npxTool "webpack-cli -p" __SOURCE_DIRECTORY__ |> ignore
 //#else
-    runTool yarnTool "webpack-cli -p" __SOURCE_DIRECTORY__
+    runTool yarnTool "webpack-cli -p" __SOURCE_DIRECTORY__ |> ignore
 //#endif
 )
 
@@ -156,9 +146,9 @@ Target.create "Run" (fun _ ->
     }
     let client = async {
 //#if (js-deps == "npm")
-        runTool npxTool "webpack-dev-server" __SOURCE_DIRECTORY__
+        runTool npxTool "webpack-dev-server" __SOURCE_DIRECTORY__ |> ignore
 //#else
-        runTool yarnTool "webpack-dev-server" __SOURCE_DIRECTORY__
+        runTool yarnTool "webpack-dev-server" __SOURCE_DIRECTORY__ |> ignore
 //#endif
     }
     let browser = async {
@@ -183,7 +173,7 @@ Target.create "Run" (fun _ ->
 //#if (deploy == "docker" || deploy == "gcp-kubernetes" || deploy == "gcp-appengine")
 let buildDocker tag =
     let args = sprintf "build -t %s ." tag
-    runTool "docker" args __SOURCE_DIRECTORY__
+    runTool "docker" args __SOURCE_DIRECTORY__ |> ignore
 
 Target.create "Bundle" (fun _ ->
     let serverDir = Path.combine deployDir "Server"
@@ -216,13 +206,13 @@ Target.create "Docker" (fun _ ->
 Target.create "Publish" (fun _ ->
     let imageName = createDockerImageName dockerImageName
     let pushArgs = sprintf "push %s" imageName
-    runTool "docker" pushArgs __SOURCE_DIRECTORY__
+    runTool "docker" pushArgs __SOURCE_DIRECTORY__ |> ignore
 )
 
 Target.create "ClusterAuth" (fun _ ->
     let clusterName = Environment.environVarOrDefault "SAFE_CLUSTER" "safe-cluster"
     let authArgs = sprintf "container clusters get-credentials %s" clusterName
-    runTool (gcloudTool ()) authArgs __SOURCE_DIRECTORY__
+    runTool (gcloudTool ()) authArgs __SOURCE_DIRECTORY__ |> ignore
 )
 
 Target.create "Deploy" (fun _ ->
@@ -321,7 +311,7 @@ Target.create "AppService" (fun _ ->
 
 //#if (deploy == "gcp-appengine")
 Target.create "Deploy" (fun _ ->
-    runTool (gcloudTool ()) "app deploy --quiet" __SOURCE_DIRECTORY__
+    runTool (gcloudTool ()) "app deploy --quiet" __SOURCE_DIRECTORY__ |> ignore
 )
 //#endif
 
@@ -355,17 +345,17 @@ Target.create "Configure" (fun args ->
     let gitTool = platformTool "git" "git.exe"
     let herokuTool = platformTool "heroku" "heroku.cmd"
     let arguments =  ("apps:create"::args.Context.Arguments) |> String.concat " "
-    let output = runToolWithOutput herokuTool arguments __SOURCE_DIRECTORY__
+    let output = runTool herokuTool arguments __SOURCE_DIRECTORY__
     let app = (output.Split '|').[0]
     printfn "app created in %s" (app.Trim())
     let appName = app.[8..(app.IndexOf(".")-1)]
-    runTool gitTool "init" __SOURCE_DIRECTORY__
+    runTool gitTool "init" __SOURCE_DIRECTORY__ |> ignore
     let gitCmd = sprintf "git:remote --app %s" appName
-    runTool herokuTool gitCmd __SOURCE_DIRECTORY__
-    runTool herokuTool "buildpacks:set -i 1 https://github.com/heroku/heroku-buildpack-nodejs" __SOURCE_DIRECTORY__
-    runTool herokuTool "buildpacks:set -i 2 https://github.com/SAFE-Stack/SAFE-buildpack" __SOURCE_DIRECTORY__
-    runTool gitTool "add ." __SOURCE_DIRECTORY__
-    runTool gitTool "commit -m initial" __SOURCE_DIRECTORY__
+    runTool herokuTool gitCmd __SOURCE_DIRECTORY__ |> ignore
+    runTool herokuTool "buildpacks:set -i 1 https://github.com/heroku/heroku-buildpack-nodejs" __SOURCE_DIRECTORY__ |> ignore
+    runTool herokuTool "buildpacks:set -i 2 https://github.com/SAFE-Stack/SAFE-buildpack" __SOURCE_DIRECTORY__ |> ignore
+    runTool gitTool "add ." __SOURCE_DIRECTORY__ |> ignore
+    runTool gitTool "commit -m initial" __SOURCE_DIRECTORY__ |> ignore
 )
 
 Target.create "Bundle" (fun _ ->
@@ -384,9 +374,9 @@ Target.create "Bundle" (fun _ ->
 
 Target.create "Deploy" (fun _ ->
     let gitTool = platformTool "git" "git.exe"
-    runTool gitTool "push heroku master" __SOURCE_DIRECTORY__
+    runTool gitTool "push heroku master" __SOURCE_DIRECTORY__ |> ignore
     let herokuTool = platformTool "heroku" "heroku.cmd"
-    runTool herokuTool "open" __SOURCE_DIRECTORY__
+    runTool herokuTool "open" __SOURCE_DIRECTORY__ |> ignore
 )
 //#endif
 open Fake.Core.TargetOperators
