bd7e67620ae692f0445e1d43905e3093df891504
Tomasz Heimowski <tomek.heimowski@gmail.com>
2020-02-25 15:00:41 +0100

    Use SAFE.Server.Environment

--- Content/src/Server/Server.fs
+++ Content/src/Server/Server.fs
@@ -1,36 +1,32 @@
 open System.IO
-open System.Threading.Tasks
 
-open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.DependencyInjection
 open FSharp.Control.Tasks.V2
 open Giraffe
 open Saturn
+
 open Shared
 
-let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+open SAFE.Server
 
-let publicPath = Path.GetFullPath "../Client/public"
+let publicPath = Environment.getEnvVar "public_path" "../Client/public" |> Path.GetFullPath
+let port = Environment.getEnvVar "PORT" "8085" |> uint16
 
-let port =
-    "PORT"
-    |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
+let webApp =
+    router {
+        get "/api/init" (fun next ctx ->
+            task {
+                let counter = { Value = 42 }
+                return! json counter next ctx
+            }) }
 
-let webApp = router {
-    get "/api/init" (fun next ctx ->
-        task {
-            let counter = {Value = 42}
-            return! json counter next ctx
-        })
-}
-
-let app = application {
-    url ("http://0.0.0.0:" + port.ToString() + "/")
-    use_router webApp
-    memory_cache
-    use_static publicPath
-    use_json_serializer(Thoth.Json.Giraffe.ThothSerializer())
-    use_gzip
-}
+let app =
+    application {
+        url ("http://0.0.0.0:" + port.ToString() + "/")
+        use_router webApp
+        memory_cache
+        use_static publicPath
+        use_json_serializer (Thoth.Json.Giraffe.ThothSerializer())
+        use_gzip
+    }
 
 run app
