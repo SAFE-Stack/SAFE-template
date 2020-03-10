7490ced2610a760c6a9a72a9915a382973047233
Oliver Coad <oliver.coad@gmail.com>
2020-03-11 01:09:31 +1100

    Fix devServerProxy context paths

--- build.fsx
+++ build.fsx
@@ -40,13 +40,13 @@ Target.create "BuildWebPackConfig" (fun _ ->
 
     let devServerProxy =
         """{
-        // redirect requests that start with /api/* to the server on port 8085
-        '/api/*': {
+        // redirect requests that start with /api/ to the server on port 8085
+        '/api/**': {
             target: 'http://localhost:' + (process.env.SERVER_PROXY_PORT || "8085"),
                changeOrigin: true
            },
-        // redirect websocket requests that start with /socket/* to the server on the port 8085
-        '/socket/*': {
+        // redirect websocket requests that start with /socket/ to the server on the port 8085
+        '/socket/**': {
             target: 'http://localhost:' + (process.env.SERVER_PROXY_PORT || "8085"),
             ws: true
            }
