8cd9db25f7e66ca3f9a5bc96cfaaf884f589cd52
Tomasz Heimowski <tomek.heimowski@gmail.com>
2020-03-31 22:06:13 +0200

    Add Server tests

---
+++ Content/tests/Server.Tests/Server.Tests.fs
@@ -0,0 +1,12 @@
+﻿module Server.Tests
+
+open Expecto
+
+let serverTests = testList "Server" [
+    test "one is one" {
+        Expect.equal 1 1 "One is one"
+    }
+]
+
+[<EntryPoint>]
+let main argv = runTests defaultConfig serverTests
