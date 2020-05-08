4918efaea368b860f3756c98722574093bb35d6e
Tomasz Heimowski <tomek.heimowski@gmail.com>
2020-05-08 22:32:54 +0200

    Remove UnitTests project

--- Content/tests/UnitTests/Client.Tests.fs
+++
@@ -1,9 +0,0 @@
-module Client.Tests
-
-open Fable.Mocha
-
-let clientTests = testList "Client" [
-    testCase "Counter is none" <| fun _ ->
-        let model, msg = Client.init ()
-        Expect.equal true model.Counter.IsSome "Counter is some"
-]
--- Content/tests/UnitTests/Server.Tests.fs
+++
@@ -1,8 +0,0 @@
-module Server.Tests
-
-open Expecto
-
-let serverTests = testList "Server" [
-    testCase "one is one" <| fun () ->
-        Expect.equal (Server.serverAdd 1 2) 3 "One is one"
-]
