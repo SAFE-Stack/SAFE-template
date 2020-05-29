ceccfb8509d1a2675704710549ee7e4abf8d4567
Isaac Abraham <isaac.abraham@gmail.com>
2020-05-29 16:45:47 +0200

    Updates

--- Content/src/Client/Client.fs
+++ Content/src/Client/Client.fs
@@ -4,19 +4,11 @@ open Elmish
 open Elmish.React
 open Fable.React
 open Fable.React.Props
+open Shared
 open Thoth.Fetch
 
-open Shared
+type Model = Counter option
 
-// The model holds data that you want to keep track of while the application is running
-// in this case, we are keeping track of a counter
-// we mark it as optional, because initially it will not be available from the client
-// the initial value will be requested from server
-type Model =
-    { Counter: Counter option }
-
-// The Msg type defines what events/actions can occur while the application is running
-// the state of the application changes *only* in reaction to these events
 type Msg =
     | Increment
     | Decrement
@@ -24,52 +16,43 @@ type Msg =
 
 let initialCounter() = Fetch.fetchAs<_, Counter> "/api/init"
 
-// defines the initial state and initial command (= side-effect) of the application
-let init(): Model * Cmd<Msg> =
-    let initialModel = { Counter = None }
+let init() =
+    let initialModel = None
     let loadCountCmd = Cmd.OfPromise.perform initialCounter () InitialCountLoaded
     initialModel, loadCountCmd
 
-// The update function computes the next state of the application based on the current state and the incoming events/messages
-// It can also run side-effects (encoded as commands) like calling the server via Http.
-// these commands in turn, can dispatch messages to which the update function will react.
-let update (msg: Msg) (currentModel: Model): Model * Cmd<Msg> =
-    match currentModel.Counter, msg with
-    | Some counter, Increment ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value + 1 } }
+let update msg model =
+    match msg, model with
+    | Increment, Some counter ->
+        let nextModel = Some { Value = counter.Value + 1 }
         nextModel, Cmd.none
-    | Some counter, Decrement ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value - 1 } }
+    | Decrement, Some counter ->
+        let nextModel = Some { counter with Value = counter.Value - 1 }
         nextModel, Cmd.none
-    | _, InitialCountLoaded initialCount ->
-        let nextModel = { Counter = Some initialCount }
+    | InitialCountLoaded initialCount, _ ->
+        let nextModel = Some initialCount
         nextModel, Cmd.none
-    | _ -> currentModel, Cmd.none
+    | _ ->
+        model, Cmd.none
 
 let show =
     function
-    | { Counter = Some counter } -> string counter.Value
-    | { Counter = None } -> "Loading..."
+    | Some counter -> string counter.Value
+    | None -> "Loading..."
 
 (*//#if minimal
-let view (model: Model) (dispatch: Msg -> unit) =
-    div
-        [ Style
-            [ TextAlign TextAlignOptions.Center
-              Padding 40 ] ]
-        [ img [ Src "favicon.png" ]
-          h1 [] [ str "SAFE Template" ]
-          h2 [] [ str (show model) ]
-          button
-              [ Style
-                  [ Margin 5
-                    Padding 10 ]
-                OnClick(fun _ -> dispatch Decrement) ] [ str "-" ]
-          button
-              [ Style
-                  [ Margin 5
-                    Padding 10 ]
-                OnClick(fun _ -> dispatch Increment) ] [ str "+" ] ]
+let view model dispatch =
+    div [ Style [ TextAlign TextAlignOptions.Center; Padding 40 ] ] [
+        img [ Src "favicon.png" ]
+        h1 [] [ str "SAFE Template" ]
+        h2 [] [ str (show model) ]
+        button [ Style [ Margin 5; Padding 10 ]; OnClick(fun _ -> dispatch Decrement) ] [
+            str "-"
+        ]
+        button [ Style [ Margin 5; Padding 10 ]; OnClick(fun _ -> dispatch Increment) ] [
+            str "+"
+        ]
+    ]
 #else*)
 open Fulma
 
