9268ff33a4b8cc4b1c3a37792265a2d0169c0fa4
Tomasz Heimowski <tomek.heimowski@gmail.com>
2021-02-22 21:31:20 +0100

    Replace Thoth.Fetch with Fable.Fetch - closes #424

--- Content/minimal/src/Client/App.fs
+++ Content/minimal/src/Client/App.fs
@@ -1,12 +1,12 @@
 module App
 
 open Browser
-open Thoth.Fetch
 
 promise {
     let header = document.getElementById "header"
     header.innerText <- "loading..."
     do! Promise.sleep 1000
-    let! message = Fetch.get Shared.Route.hello
-    header.innerText <- message
+    let! response = Fetch.fetch Shared.Route.hello []
+    let! text = response.json<string>()
+    header.innerText <- text
 } |> Promise.start
