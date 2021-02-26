1154faacb10842fb4eb83438289061ebf33da992
nojaf <florian.verdonck@outlook.com>
2021-02-26 13:29:26 +0100

    Scope formatting to src folder.

--- Content/default/Build.fs
+++ Content/default/Build.fs
@@ -4,9 +4,7 @@ open Fake.IO
 open Farmer
 open Farmer.Builders
 
-let execContext =
-    Context.FakeExecutionContext.Create false "build.fsx" []
-
+let execContext = Context.FakeExecutionContext.Create false "build.fsx" [ ]
 Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
 
 let sharedPath = Path.getFullName "src/Shared"
@@ -21,14 +19,13 @@ let npm args workingDir =
         match ProcessUtils.tryFindFileOnPath "npm" with
         | Some path -> path
         | None ->
-            "npm was not found in path. Please install it and make sure it's available from your path. "
-            + "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+            "npm was not found in path. Please install it and make sure it's available from your path. " +
+            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
             |> failwith
 
-    let arguments =
-        args |> String.split ' ' |> Arguments.OfArgs
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
 
-    Command.RawCommand(npmPath, arguments)
+    Command.RawCommand (npmPath, arguments)
     |> CreateProcess.fromCommand
     |> CreateProcess.withWorkingDirectory workingDir
     |> CreateProcess.ensureExitCode
@@ -36,83 +33,75 @@ let npm args workingDir =
     |> ignore
 
 let dotnet cmd workingDir =
-    let result =
-        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-
-    if result.ExitCode <> 0 then
-        failwithf "'dotnet %s' failed in %s" cmd workingDir
+    let result = DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
 
 
 Target.create "Clean" (fun _ -> Shell.cleanDir deployDir)
 
 Target.create "InstallClient" (fun _ -> npm "install" ".")
 
-Target.create
-    "Bundle"
-    (fun _ ->
-        dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
-        dotnet "fable --run webpack -p" clientPath)
+Target.create "Bundle" (fun _ ->
+    dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
+    dotnet "fable --run webpack -p" clientPath
+)
 
-Target.create
-    "Azure"
-    (fun _ ->
-        let web =
-            webApp {
-                name "SAFE.App"
-                zip_deploy "deploy"
-            }
+Target.create "Azure" (fun _ ->
+    let web = webApp {
+        name "SAFE.App"
+        zip_deploy "deploy"
+    }
+    let deployment = arm {
+        location Location.WestEurope
+        add_resource web
+    }
 
-        let deployment =
-            arm {
-                location Location.WestEurope
-                add_resource web
-            }
+    deployment
+    |> Deploy.execute "SAFE.App" Deploy.NoParameters
+    |> ignore
+)
 
-        deployment
-        |> Deploy.execute "SAFE.App" Deploy.NoParameters
-        |> ignore)
+Target.create "Run" (fun _ ->
+    dotnet "build" sharedPath
+    [ async { dotnet "watch run" serverPath }
+      async { dotnet "fable watch --run webpack-dev-server" clientPath } ]
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+)
 
-Target.create
-    "Run"
-    (fun _ ->
-        dotnet "build" sharedPath
+Target.create "RunTests" (fun _ ->
+    dotnet "build" sharedTestsPath
+    [ async { dotnet "watch run" serverTestsPath }
+      async { dotnet "fable watch --run webpack-dev-server --config ../../webpack.tests.config.js" "tests/Client" } ]
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+)
 
-        [ async { dotnet "watch run" serverPath }
-          async { dotnet "fable watch --run webpack-dev-server" clientPath } ]
-        |> Async.Parallel
-        |> Async.RunSynchronously
-        |> ignore)
+Target.create "Format" (fun _ ->
+    let result = DotNet.exec id "fantomas" "src -r"
 
-Target.create
-    "RunTests"
-    (fun _ ->
-        dotnet "build" sharedTestsPath
-
-        [ async { dotnet "watch run" serverTestsPath }
-          async { dotnet "fable watch --run webpack-dev-server --config ../../webpack.tests.config.js" "tests/Client" } ]
-        |> Async.Parallel
-        |> Async.RunSynchronously
-        |> ignore)
-
-Target.create
-    "Format"
-    (fun _ ->
-        let result = DotNet.exec id "fantomas" ". -r"
-
-        if not result.OK then
-            printfn "Errors while formatting all files: %A" result.Messages)
+    if not result.OK then
+        printfn "Errors while formatting all files: %A" result.Messages
+)
 
 open Fake.Core.TargetOperators
 
-let dependencies =
-    [ "Clean"
-      ==> "InstallClient"
-      ==> "Bundle"
-      ==> "Azure"
+let dependencies = [
+    "Clean"
+        ==> "InstallClient"
+        ==> "Bundle"
+        ==> "Azure"
 
-      "Clean" ==> "InstallClient" ==> "Run"
+    "Clean"
+        ==> "InstallClient"
+        ==> "Run"
 
-      "Clean" ==> "InstallClient" ==> "RunTests" ]
+    "Clean"
+        ==> "InstallClient"
+        ==> "RunTests"
+]
 
 [<EntryPoint>]
 let main args =
@@ -120,7 +109,6 @@ let main args =
         match args with
         | [| target |] -> Target.runOrDefault target
         | _ -> Target.runOrDefault "Run"
-
         0
     with e ->
         printfn "%A" e
--- Content/default/tests/Shared/Shared.Tests.fs
+++ Content/default/tests/Shared/Shared.Tests.fs
@@ -10,12 +10,9 @@ open Expecto
 
 open Shared
 
-let shared =
-    testList
-        "Shared"
-        [ testCase
-              "Empty string is not a valid description"
-              (fun _ ->
-                  let expected = false
-                  let actual = Todo.isValid ""
-                  Expect.equal actual expected "Should be false") ]
+let shared = testList "Shared" [
+    testCase "Empty string is not a valid description" <| fun _ ->
+        let expected = false
+        let actual = Todo.isValid ""
+        Expect.equal actual expected "Should be false"
+]
