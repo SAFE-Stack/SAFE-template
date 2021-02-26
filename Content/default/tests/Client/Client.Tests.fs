0d94c10385e50a583ec7e78fe9ef2dbf08a588fc
nojaf <florian.verdonck@outlook.com>
2021-02-26 13:16:26 +0100

    Change unit tests

--- Content/default/tests/Client/Client.Tests.fs
+++ Content/default/tests/Client/Client.Tests.fs
@@ -8,15 +8,16 @@ open Shared
 let client =
     testList
         "Client"
-        [ testCase "Added todo"
-          <| fun _ ->
-              let newTodo = Todo.create "new todo"
-              let model, _ = init ()
+        [ testCase
+              "Added todo"
+              (fun _ ->
+                  let newTodo = Todo.create "new todo"
+                  let model, _ = init ()
 
-              let model, _ = update (AddedTodo newTodo) model
+                  let model, _ = update (AddedTodo newTodo) model
 
-              Expect.equal 1 model.Todos.Length "There should be 1 todo"
-              Expect.equal newTodo model.Todos.[0] "Todo should equal new todo" ]
+                  Expect.equal 1 model.Todos.Length "There should be 1 todo"
+                  Expect.equal newTodo model.Todos.[0] "Todo should equal new todo") ]
 
 let all =
     testList
--- Content/default/tests/Shared/Shared.Tests.fs
+++ Content/default/tests/Shared/Shared.Tests.fs
@@ -13,8 +13,9 @@ open Shared
 let shared =
     testList
         "Shared"
-        [ testCase "Empty string is not a valid description"
-          <| fun _ ->
-              let expected = false
-              let actual = Todo.isValid ""
-              Expect.equal actual expected "Should be false" ]
+        [ testCase
+              "Empty string is not a valid description"
+              (fun _ ->
+                  let expected = false
+                  let actual = Todo.isValid ""
+                  Expect.equal actual expected "Should be false") ]
