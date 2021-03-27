f3d46a49476c6bc5d18d4f6d1c5cbead60059969
Tomasz Heimowski <tomek.heimowski@gmail.com>
2021-03-27 18:17:41 +0100

    Replace Dotnet.exec with direct dotnet call

--- Content/default/Build.fs
+++ Content/default/Build.fs
@@ -1,5 +1,4 @@
 open Fake.Core
-open Fake.DotNet
 open Fake.IO
 open Farmer
 open Farmer.Builders
@@ -14,37 +13,20 @@ let deployDir = Path.getFullName "deploy"
 let sharedTestsPath = Path.getFullName "tests/Shared"
 let serverTestsPath = Path.getFullName "tests/Server"
 
-let npm args workingDir =
-    let npmPath =
-        match ProcessUtils.tryFindFileOnPath "npm" with
-        | Some path -> path
-        | None ->
-            "npm was not found in path. Please install it and make sure it's available from your path. " +
-            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
-            |> failwith
-
-    let arguments = args |> String.split ' ' |> Arguments.OfArgs
-
-    Command.RawCommand (npmPath, arguments)
-    |> CreateProcess.fromCommand
-    |> CreateProcess.withWorkingDirectory workingDir
+let run exe arg dir =
+    CreateProcess.fromRawCommandLine exe arg
+    |> CreateProcess.withWorkingDirectory dir
     |> CreateProcess.ensureExitCode
     |> Proc.run
     |> ignore
 
-let dotnet cmd workingDir =
-    let result = DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-    if not result.OK then
-        Trace.traceErrorfn "Errors while executing 'dotnet %s': %A" cmd result.Messages
-        failwithf "'dotnet %s' failed in %s" cmd workingDir
-
 Target.create "Clean" (fun _ -> Shell.cleanDir deployDir)
 
-Target.create "InstallClient" (fun _ -> npm "install" ".")
+Target.create "InstallClient" (fun _ -> run "npm" "install" ".")
 
 Target.create "Bundle" (fun _ ->
-    dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
-    dotnet "fable --run webpack -p" clientPath
+    run "dotnet" (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
+    run "dotnet" "fable --run webpack -p" clientPath
 )
 
 Target.create "Azure" (fun _ ->
@@ -63,25 +45,25 @@ Target.create "Azure" (fun _ ->
 )
 
 Target.create "Run" (fun _ ->
-    dotnet "build" sharedPath
-    [ async { dotnet "watch run" serverPath }
-      async { dotnet "fable watch --run webpack-dev-server" clientPath } ]
+    run "dotnet" "build" sharedPath
+    [ async { run "dotnet" "watch run" serverPath }
+      async { run "dotnet" "fable watch --run webpack-dev-server" clientPath } ]
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
 )
 
 Target.create "RunTests" (fun _ ->
-    dotnet "build" sharedTestsPath
-    [ async { dotnet "watch run" serverTestsPath }
-      async { dotnet "fable watch --run webpack-dev-server --config ../../webpack.tests.config.js" "tests/Client" } ]
+    run "dotnet" "build" sharedTestsPath
+    [ async { run "dotnet" "watch run" serverTestsPath }
+      async { run "dotnet" "fable watch --run webpack-dev-server --config ../../webpack.tests.config.js" "tests/Client" } ]
     |> Async.Parallel
     |> Async.RunSynchronously
     |> ignore
 )
 
 Target.create "Format" (fun _ ->
-    dotnet "fantomas . -r" "src"
+    run "dotnet" "fantomas . -r" "src"
 )
 
 open Fake.Core.TargetOperators
