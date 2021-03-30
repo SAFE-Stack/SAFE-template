912117e82bfce58475233d1ce347525e23d08a4c
Tomasz Heimowski <tomek.heimowski@gmail.com>
2021-03-30 22:29:54 +0200

    Add Helpers to Build projecT

--- Content/default/Build.fs
+++ Content/default/Build.fs
@@ -3,30 +3,26 @@ open Fake.IO
 open Farmer
 open Farmer.Builders
 
-let execContext = Context.FakeExecutionContext.Create false "build.fsx" [ ]
-Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
+open Helpers
+
+initializeContext()
 
 let sharedPath = Path.getFullName "src/Shared"
 let serverPath = Path.getFullName "src/Server"
 let clientPath = Path.getFullName "src/Client"
-let deployDir = Path.getFullName "deploy"
+let deployPath = Path.getFullName "deploy"
 let sharedTestsPath = Path.getFullName "tests/Shared"
 let serverTestsPath = Path.getFullName "tests/Server"
+let clientTestsPath = Path.getFullName "tests/Client"
 
-let run exe arg dir =
-    CreateProcess.fromRawCommandLine exe arg
-    |> CreateProcess.withWorkingDirectory dir
-    |> CreateProcess.ensureExitCode
-    |> Proc.run
-    |> ignore
+Target.create "Clean" (fun _ -> Shell.cleanDir deployPath)
 
-Target.create "Clean" (fun _ -> Shell.cleanDir deployDir)
-
-Target.create "InstallClient" (fun _ -> run "npm" "install" ".")
+Target.create "InstallClient" (fun _ -> run npm "install" ".")
 
 Target.create "Bundle" (fun _ ->
-    run "dotnet" (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
-    run "dotnet" "fable --run webpack -p" clientPath
+    [ "server", dotnet $"publish -c Release -o \"{deployPath}\"" serverPath
+      "client", dotnet "fable --run webpack -p" clientPath ]
+    |> runParallel
 )
 
 Target.create "Azure" (fun _ ->
@@ -45,25 +41,21 @@ Target.create "Azure" (fun _ ->
 )
 
 Target.create "Run" (fun _ ->
-    run "dotnet" "build" sharedPath
-    [ async { run "dotnet" "watch run" serverPath }
-      async { run "dotnet" "fable watch --run webpack-dev-server" clientPath } ]
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> ignore
+    run dotnet "build" sharedPath
+    [ "server", dotnet "watch run" serverPath
+      "client", dotnet "fable watch --run webpack-dev-server" clientPath ]
+    |> runParallel
 )
 
 Target.create "RunTests" (fun _ ->
-    run "dotnet" "build" sharedTestsPath
-    [ async { run "dotnet" "watch run" serverTestsPath }
-      async { run "dotnet" "fable watch --run webpack-dev-server --config ../../webpack.tests.config.js" "tests/Client" } ]
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> ignore
+    run dotnet "build" sharedTestsPath
+    [ "server", dotnet "watch run" serverTestsPath
+      "client", dotnet "fable watch --run webpack-dev-server --config ../../webpack.tests.config.js" clientTestsPath ]
+    |> runParallel
 )
 
 Target.create "Format" (fun _ ->
-    run "dotnet" "fantomas . -r" "src"
+    run dotnet "fantomas . -r" "src"
 )
 
 open Fake.Core.TargetOperators
@@ -84,12 +76,4 @@ let dependencies = [
 ]
 
 [<EntryPoint>]
-let main args =
-    try
-        match args with
-        | [| target |] -> Target.runOrDefault target
-        | _ -> Target.runOrDefault "Run"
-        0
-    with e ->
-        printfn "%A" e
-        1
+let main args = runOrDefault args
