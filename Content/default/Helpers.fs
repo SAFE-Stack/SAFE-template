dd63b5b3b1305fc087e647ab57acc90217b57cbc
Tomasz Heimowski <tomek.heimowski@gmail.com>
2021-04-15 20:27:09 +0200

    Bring back tryFindFileOnPath for npm

--- Content/default/Helpers.fs
+++ Content/default/Helpers.fs
@@ -72,7 +72,16 @@ let createProcess exe arg dir =
     |> CreateProcess.ensureExitCode
 
 let dotnet = createProcess "dotnet"
-let npm = createProcess "npm"
+let npm =
+    let npmPath =
+        match ProcessUtils.tryFindFileOnPath "npm" with
+        | Some path -> path
+        | None ->
+            "npm was not found in path. Please install it and make sure it's available from your path. " +
+            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+            |> failwith
+
+    createProcess npmPath
 
 let run proc arg dir =
     proc arg dir
