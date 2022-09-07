c3ef0e12c8a3384ed082ed80e6a5b4af041264b8
Prashant Pathak <theprash@gmail.com>
2022-09-07 17:09:10 +0100

    Set OS to Windows explicitly

--- Content/default/Build.fs
+++ Content/default/Build.fs
@@ -31,6 +31,7 @@ Target.create "Bundle" (fun _ ->
 Target.create "Azure" (fun _ ->
     let web = webApp {
         name "SAFE.App"
+        operating_system OS.Windows
         runtime_stack Runtime.DotNet60
         zip_deploy "deploy"
     }
