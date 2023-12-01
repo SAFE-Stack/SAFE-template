cee1acc173842259261066be7510359902a55803
Martin Bryant <mail@martinbryant.dev>
2023-12-01 11:18:34 +0000

    used className attribute for styles in Index.fs

--- Content/default/src/Client/Index.fs
+++ Content/default/src/Client/Index.fs
@@ -43,22 +43,11 @@ open Feliz
 
 let private todoAction model dispatch =
     Html.div [
-        prop.classes [ "flex"; "flex-col"; "sm:flex-row"; "mt-4"; "gap-4" ]
+        prop.className "flex flex-col sm:flex-row mt-4 gap-4"
         prop.children [
             Html.input [
-                prop.classes [
-                    "shadow"
-                    "appearance-none"
-                    "border"
-                    "rounded"
-                    "w-full"
-                    "py-2"
-                    "px-3"
-                    "outline-none"
-                    "focus:ring-2"
-                    "ring-teal-300"
-                    "text-grey-darker"
-                ]
+                prop.className
+                    "shadow appearance-none border rounded w-full py-2 px-3 outline-none focus:ring-2 ring-teal-300 text-grey-darker"
                 prop.value model.Input
                 prop.placeholder "What needs to be done?"
                 prop.autoFocus true
@@ -68,21 +57,8 @@ let private todoAction model dispatch =
                         dispatch AddTodo)
             ]
             Html.button [
-                prop.classes [
-                    "flex-no-shrink"
-                    "p-2"
-                    "px-12"
-                    "rounded"
-                    "bg-teal-600"
-                    "outline-none"
-                    "focus:ring-2"
-                    "ring-teal-300"
-                    "font-bold"
-                    "text-white"
-                    "hover:bg-teal"
-                    "disabled:opacity-30"
-                    "disabled:cursor-not-allowed"
-                ]
+                prop.className
+                    "flex-no-shrink p-2 px-12 rounded bg-teal-600 outline-none focus:ring-2 ring-teal-300 font-bold text-white hover:bg-teal disabled:opacity-30 disabled:cursor-not-allowed"
                 prop.disabled (Todo.isValid model.Input |> not)
                 prop.onClick (fun _ -> dispatch AddTodo)
                 prop.text "Add"
@@ -92,18 +68,10 @@ let private todoAction model dispatch =
 
 let private todoList model dispatch =
     Html.div [
-        prop.classes [
-            "bg-white/80"
-            "rounded-md"
-            "shadow-md"
-            "p-4"
-            "w-5/6"
-            "lg:w-3/4"
-            "lg:max-w-2xl"
-        ]
+        prop.className "bg-white/80 rounded-md shadow-md p-4 w-5/6 lg:w-3/4 lg:max-w-2xl"
         prop.children [
             Html.ol [
-                prop.className [ "list-decimal"; "ml-6" ]
+                prop.className "list-decimal ml-6"
                 prop.children [
                     for todo in model.Todos do
                         Html.li [ prop.className "my-1"; prop.text todo.Description ]
@@ -116,7 +84,7 @@ let private todoList model dispatch =
 
 let view model dispatch =
     Html.section [
-        prop.classes [ "h-screen"; "w-screen" ]
+        prop.className "h-screen w-screen"
         prop.style [
             style.backgroundSize "cover"
             style.backgroundImageUrl "https://unsplash.it/1200/900?random"
@@ -126,32 +94,15 @@ let view model dispatch =
         prop.children [
             Html.a [
                 prop.href "https://safe-stack.github.io/"
-                prop.classes [
-                    "absolute"
-                    "block"
-                    "ml-12"
-                    "h-12"
-                    "w-12"
-                    "bg-teal-300"
-                    "hover:cursor-pointer"
-                    "hover:bg-teal-400"
-                ]
+                prop.className "absolute block ml-12 h-12 w-12 bg-teal-300 hover:cursor-pointer hover:bg-teal-400"
                 prop.children [ Html.img [ prop.src "/favicon.png"; prop.alt "Logo" ] ]
             ]
 
             Html.div [
-                prop.classes [ "flex"; "flex-col"; "items-center"; "justify-center"; "h-full" ]
+                prop.className "flex flex-col items-center justify-center h-full"
                 prop.children [
                     Html.h1 [
-                        prop.classes [
-                            "text-center"
-                            "text-5xl"
-                            "font-bold"
-                            "text-white"
-                            "mb-3"
-                            "rounded-md"
-                            "p-4"
-                        ]
+                        prop.className "text-center text-5xl font-bold text-white mb-3 rounded-md p-4"
                         prop.text "SAFE.App"
                     ]
                     todoList model dispatch
