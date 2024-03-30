248941aef8c5be38580eecde4b34f1ae244a7d27
Isaac Abraham <isaac.abraham@gmail.com>
2024-03-30 17:49:02 +0000

    Update to meta b4

--- Content/default/src/Client/Index.fs
+++ Content/default/src/Client/Index.fs
@@ -11,83 +11,84 @@ type Model = {
 
 type Msg =
     | SetInput of string
-    | GetTodos of ApiCall<unit, Todo list>
-    | AddTodo of ApiCall<unit, Todo>
+    | LoadTodos of ApiCall<unit, Todo list>
+    | SaveTodo of ApiCall<unit, Todo>
 
 let todosApi = Api.makeProxy<ITodosApi> ()
 
 let init () =
     let model = { Todos = NotStarted; Input = "" }
-    model, Cmd.ofMsg (GetTodos(Start()))
+    model, Cmd.ofMsg (LoadTodos(Start()))
 
 let update msg model =
     match msg with
     | SetInput value -> { model with Input = value }, Cmd.none
-    | GetTodos msg ->
+    | LoadTodos msg ->
         match msg with
         | Start() ->
-            let cmd = Cmd.OfAsync.perform todosApi.getTodos () (Finished >> GetTodos)
+            let cmd = ApiCall.execute (todosApi.getTodos, (), LoadTodos)
             { model with Todos = Loading }, cmd
         | Finished todos -> { model with Todos = Loaded todos }, Cmd.none
-    | AddTodo msg ->
+    | SaveTodo msg ->
         match msg with
         | Start() ->
             let todo = Todo.create model.Input
-            let cmd = Cmd.OfAsync.perform todosApi.addTodo todo (Finished >> AddTodo)
+            let cmd = ApiCall.execute (todosApi.addTodo, todo, SaveTodo)
             { model with Input = "" }, cmd
         | Finished todo ->
             {
                 model with
-                    Todos = model.Todos.Map(fun todos -> todos @ [ todo ])
+                    Todos = model.Todos |> RemoteData.map (fun todos -> todos @ [ todo ])
             },
             Cmd.none
 
 open Feliz
 
-let private todoAction model dispatch =
-    Html.div [
-        prop.className "flex flex-col sm:flex-row mt-4 gap-4"
-        prop.children [
-            Html.input [
-                prop.className
-                    "shadow appearance-none border rounded w-full py-2 px-3 outline-none focus:ring-2 ring-teal-300 text-grey-darker"
-                prop.value model.Input
-                prop.placeholder "What needs to be done?"
-                prop.autoFocus true
-                prop.onChange (SetInput >> dispatch)
-                prop.onKeyPress (fun ev ->
-                    if ev.key = "Enter" then
-                        dispatch (AddTodo(Start())))
-            ]
-            Html.button [
-                prop.className
-                    "flex-no-shrink p-2 px-12 rounded bg-teal-600 outline-none focus:ring-2 ring-teal-300 font-bold text-white hover:bg-teal disabled:opacity-30 disabled:cursor-not-allowed"
-                prop.disabled (Todo.isValid model.Input |> not)
-                prop.onClick (fun _ -> dispatch (AddTodo(Start())))
-                prop.text "Add"
-            ]
-        ]
-    ]
-
-let private todoList model dispatch =
-    Html.div [
-        prop.className "bg-white/80 rounded-md shadow-md p-4 w-5/6 lg:w-3/4 lg:max-w-2xl"
-        prop.children [
-            Html.ol [
-                prop.className "list-decimal ml-6"
-                prop.children [
-                    match model.Todos with
-                    | NotStarted -> Html.text "Not Started."
-                    | Loading -> Html.text "Loading..."
-                    | Loaded todos ->
-                        for todo in todos do
-                            Html.li [ prop.className "my-1"; prop.text todo.Description ]
+module ViewComponents =
+    let todoAction model dispatch =
+        Html.div [
+            prop.className "flex flex-col sm:flex-row mt-4 gap-4"
+            prop.children [
+                Html.input [
+                    prop.className
+                        "shadow appearance-none border rounded w-full py-2 px-3 outline-none focus:ring-2 ring-teal-300 text-grey-darker"
+                    prop.value model.Input
+                    prop.placeholder "What needs to be done?"
+                    prop.autoFocus true
+                    prop.onChange (SetInput >> dispatch)
+                    prop.onKeyPress (fun ev ->
+                        if ev.key = "Enter" then
+                            dispatch (SaveTodo(Start())))
+                ]
+                Html.button [
+                    prop.className
+                        "flex-no-shrink p-2 px-12 rounded bg-teal-600 outline-none focus:ring-2 ring-teal-300 font-bold text-white hover:bg-teal disabled:opacity-30 disabled:cursor-not-allowed"
+                    prop.disabled (Todo.isValid model.Input |> not)
+                    prop.onClick (fun _ -> dispatch (SaveTodo(Start())))
+                    prop.text "Add"
                 ]
             ]
-
-            todoAction model dispatch
         ]
-    ]
+
+    let todoList model dispatch =
+        Html.div [
+            prop.className "bg-white/80 rounded-md shadow-md p-4 w-5/6 lg:w-3/4 lg:max-w-2xl"
+            prop.children [
+                Html.ol [
+                    prop.className "list-decimal ml-6"
+                    prop.children [
+                        match model.Todos with
+                        | NotStarted -> Html.text "Not Started."
+                        | Loading -> Html.text "Loading..."
+                        | Loaded todos ->
+                            for todo in todos do
+                                Html.li [ prop.className "my-1"; prop.text todo.Description ]
+                    ]
+                ]
+
+                todoAction model dispatch
+            ]
+        ]
 
 let view model dispatch =
     Html.section [
@@ -112,7 +113,7 @@ let view model dispatch =
                         prop.className "text-center text-5xl font-bold text-white mb-3 rounded-md p-4"
                         prop.text "SAFE.App"
                     ]
-                    todoList model dispatch
+                    ViewComponents.todoList model dispatch
                 ]
             ]
         ]
